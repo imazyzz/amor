ed73ad3f3d5c099e724a62b835c6b19467bac7f8
Felipe Assis <felipeassis@MacBook-de-Felipe.local>
2024-12-11 00:59:46 -0300

    New Feature

--- script.js
+++ script.js
@@ -20,3 +20,61 @@ function updateCounter() {
 updateCounter();
 
 setInterval(updateCounter, 24 * 60 * 60 * 1000);
+
+const frases = [
+    "“Sometimes the smallest decisions can change your life forever.”",
+    "“You had me at hello.”",
+    "“To me, you are perfect.”",
+    "“If you're a bird, I'm a bird.”",
+    "“You complete me.”",
+    "“Our love is like the wind. I can't see it, but I can feel it.”",
+    "“As you wish.”",
+    "“It was always you.”",
+];
+
+const elementoCitacao = document.querySelector(".citacao");
+let indiceAtual = 0;
+
+function alterarFrase() {
+    elementoCitacao.classList.remove("show");
+
+    setTimeout(() => {
+        elementoCitacao.textContent = frases[indiceAtual];
+        indiceAtual = (indiceAtual + 1) % frases.length;
+
+        elementoCitacao.classList.add("show");
+    }, 1000);
+}
+
+setInterval(alterarFrase, 5000);
+
+elementoCitacao.textContent = frases[indiceAtual];
+elementoCitacao.classList.add("show");
+indiceAtual++;
+
+const imagens = [
+    "wpp01.jpeg",
+    "wpp02.jpeg",
+    "wpp03.jpeg",
+    "wpp04.jpeg",
+    "wpp05.jpeg",
+    "wpp06.jpeg",
+];
+
+const elementoImagem = document.querySelector(".hero-img");
+let indAtual = 0;
+
+function alterarImagem() {
+    elementoImagem.classList.remove("show");
+
+    setTimeout(() => {
+        elementoImagem.src = imagens[indAtual];
+        indAtual = (indAtual + 1) % imagens.length;
+
+        elementoImagem.classList.add("show");
+    }, 1000);
+}
+
+setInterval(alterarImagem, 5000);
+
+elementoImagem.classList.add("show");
